7ed7e3429e1779d6eb6027a54cee8e14de5ff7c0
mehmetsahin <mehmetsahinn2002@gmail.com>
2024-03-10 11:49:25 +0300

    Swagger API ye entegre etmeye çalıştım.

--- src/main/java/com/github/mehmetsahinnn/onlineordertrackingsystem/security/SecurityConfig.java
+++ src/main/java/com/github/mehmetsahinnn/onlineordertrackingsystem/security/SecurityConfig.java
@@ -16,7 +16,11 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/api/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs"
+                                ).permitAll()
                         .anyRequest().permitAll()
                 );
         return http.build();
