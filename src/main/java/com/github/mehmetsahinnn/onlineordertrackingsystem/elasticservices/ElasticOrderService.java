672fdb23c5aa120d248440d5bb297c9b077517ef
mehmetsahin <mehmetsahinn2002@gmail.com>
2024-05-21 21:28:39 +0300

    21 Mayıs Elastic Search çabaları

--- src/main/java/com/github/mehmetsahinnn/onlineordertrackingsystem/elasticservices/ElasticOrderService.java
+++ src/main/java/com/github/mehmetsahinnn/onlineordertrackingsystem/elasticservices/ElasticOrderService.java
@@ -1,11 +1,10 @@
-package com.github.mehmetsahinnn.onlineordertrackingsystem.services;
+package com.github.mehmetsahinnn.onlineordertrackingsystem.elasticservices;
 
 import com.github.mehmetsahinnn.onlineordertrackingsystem.elasticdocuments.OrderDocument;
 import com.github.mehmetsahinnn.onlineordertrackingsystem.elasticrepos.OrderDocumentRepository;
 import com.github.mehmetsahinnn.onlineordertrackingsystem.models.Order;
 import com.github.mehmetsahinnn.onlineordertrackingsystem.repositories.OrderRepository;
 import org.elasticsearch.ResourceNotFoundException;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
